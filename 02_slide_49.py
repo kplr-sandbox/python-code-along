9675afc09ee3404d5e46a6ecb3c0adbcb567352d
yahia-kplr <yahia.maamar@kplr.fr>
2023-02-09 09:26:26 +0100

    seconde slide

---
+++ 02_slide_49.py
@@ -0,0 +1,12 @@
+# Déclaration de la variable
+feedback = "A fantastic place!"
+
+# calculer la taille de la chaine de caratére
+lenght = len(feedback)
+
+#Imprimer la taille de la chaine de caractére
+print(lenght)
+
+# Imprimer le type de la variable
+
+print(type(lenght))
